8adb723f85cc92630530d6fa5ffaf83b5b3fcd02
Thorsen Kristufek <tkael.1230@gmail.com>
2024-03-16 23:10:18 -0700

    queries

--- queries.sql
+++ queries.sql
@@ -22,3 +22,46 @@ SELECT DISTINCT b.u_id, b.college_name
 FROM basic_college_info b
 JOIN sports_programs sp1 ON b.u_id = sp1.u_id AND sp1.sport = 'MFB'
 JOIN sports_programs sp2 ON b.u_id = sp2.u_id AND sp2.sport = 'MBB';
+
+-- Low admin
+SELECT college_name, ug_pop, admission_rate, city, state_abbr
+FROM basic_college_info
+WHERE admission_rate IS NOT NULL AND admission_rate <> 0
+ORDER BY admission_rate
+LIMIT 10;
+
+--Outputs Men's only colleges
+SELECT college_name, ug_pop, admission_rate, city, state_abbr
+FROM basic_college_info
+WHERE men_only = 1
+ORDER BY college_name;
+
+--Outputs women's only colleges
+SELECT college_name, ug_pop, admission_rate, city, state_abbr
+FROM basic_college_info
+WHERE women_only = 1
+ORDER BY college_name;
+
+--Colleges with Sports programs in a user
+--input Acceptance Rate range
+SELECT college_name, ug_pop, admission_rate, city, state_abbr, count_sports(u_id)
+FROM basic_college_info
+WHERE count_sports(u_id) > 0 AND admission_rate BETWEEN '%s' AND '%s'
+ORDER BY college_name;
+
+--Colleges within pop range given
+SELECT b.college_name, b.ug_pop, b.admission_rate, b.city, b.state_abbr, 
+c.population
+FROM basic_college_info b
+JOIN city_pop c ON b.city = c.city AND b.state_abbr = c.state_abbr
+WHERE c.population BETWEEN '%s' AND '%s'
+ORDER BY c.population;
+
+-- Outputs detailed sports info
+-- about a user input college
+
+SELECT sp.sport, sgr.fed_grad_rate
+FROM sports_programs sp
+JOIN basic_college_info bci ON sp.u_id = bci.u_id
+JOIN sports_grad_rate sgr ON sp.sport = sgr.sport
+WHERE bci.college_name = '%s';
